d6c43b042e85b139e65e75606cb57ffaeebcde61
Marcx <202213862k@utea.edu.pe>
2024-01-15 09:43:41 -0500

    Primer commit

---
+++ colores.java
@@ -0,0 +1,11 @@
+public class colores {
+ public static void main(String [] argus){
+    System.out.print("\033[33m mandarina");
+    System.out.print("\033[32m hierba");
+    System.out.print("\033[31m tomate");
+    System.out.print("\033[37m sabanas");
+    System.out.print("\033[36m cielo");
+    System.out.print("\033[35m nazareno");
+    System.out.print("\033[34m mar");
+ }
+}
