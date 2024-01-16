259b41fe78e76f7954be38e68369a55da353b960
Marcx <202213862k@utea.edu.pe>
2024-01-16 12:46:22 -0500

    Ejercicio6

---
+++ Ejercicios01/Ejercicio6.java
@@ -0,0 +1,13 @@
+package Ejercicios01;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+    
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+      }
+    }
+
