fe8afe09d2fab4e4c5a79e8ecd64af2f7aa4aed7
Marcx <202213862k@utea.edu.pe>
2024-01-16 12:11:23 -0500

    Ejercicio3

--- Ejercicios01/Ejercicio3.java
+++ Ejercicios01/Ejercicio3.java
@@ -1,11 +1,20 @@
 package Ejercicios01;
 public class Ejercicio3 {
     public static void main(String[] args){
-        System.out.println( "Palabras   Traduccion" );
-    System.out.println("-----------------------------------------");
-    System.out.printf("%-10s  %8.2f       %6d\n", "", 4.5,     10);
-    System.out.printf("%-10s  %8.2f       %6d\n", "peras", 2.75,     120);
-    System.out.printf("%-10s  %8.2f       %6d\n", "aguacates", 10.0,     6);
+            System.out.println("Ingles      Español");
+            System.out.println("-----------------------------------------");
+            System.out.printf("%-10s      %-10s\n",  "Computer",  "ordenador");
+            System.out.printf("%-10s      %-10s\n",  "student",  "alumno");
+            System.out.printf("%-10s      %-10s\n",  "cat", "gato");
+            System.out.printf("%-10s      %-10s\n",  "penguin", "pingüino");
+            System.out.printf("%-10s      %-10s\n",  "machine", "máquina");
+            System.out.printf("%-10s      %-10s\n",  "nature", "naturaleza");
+            System.out.printf("%-10s      %-10s\n",  "light", "luz");
+            System.out.printf("%-10s      %-10s\n",  "green", "verde");
+            System.out.printf("%-10s      %-10s\n",  "book", "libro");
+            System.out.printf("%-10s      %-10s\n",  "pyramid", "pirámide");
+            
+
       }
       }
-  
+      
