a0e48179ddb43d096c313fb60268210ef0e681a9
Marcx <202213862k@utea.edu.pe>
2024-03-04 10:12:39 -0500

    array

--- ARRAY/Array03.java
+++ ARRAY/Array03.java
@@ -4,11 +4,14 @@ public class Array03 {
     public static void main(String[] args) {
         //arreglo de notas
         double[] notas = new double[5];
+        double suma = 0;
         System.out.println("ingtrese3 la nota de la parcial ");
-        
+
         for (int i = 0; i < notas.length; i++) {
            System.out.print("nota del estudiante nº "+(i+1)+": "); 
            notas[i] = Double.parseDouble(System.console().readLine());
+           suma = suma + notas[i];
         }
+        System.out.println("El promedio del aula es: "+suma / notas.length);
     }
 }
